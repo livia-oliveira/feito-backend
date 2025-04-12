f3bbd883d09994c0fc326a4a1d7afa4d13305ffe
livia-oliveira <livia0liveiraslv93@gmail.com>
2025-04-12 00:12:38 -0300

    Criando as rotas de tarefas e subtarefas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello-world', function () {
-    return "Oiii";
-});
+Route::post('/tarefas', [TarefasController::class, 'store'])->name('tarefas.store');
+Route::get('/tarefas/{id}', [TarefasController::class, 'show'])->name('tarefas.show');
+Route::put('/tarefas/{id}', [TarefasController::class, 'update'])->name('tarefas.update');
+Route::delete('tarefas/{id}', [TarefasController::class, 'destroy'])->name('tarefas.destroy');
+
+Route::post('/subtarefas', [SubtarefasController::class, 'store'])->name('subtarefas.store');
+Route::get('/subtarefas/{id}', [SubtarefasController::class, 'show'])->name('subtarefas.show');
+Route::put('/subtarefas/{id}', [SubtarefasController::class, 'update'])->name('subtarefas.update');
+Route::delete('subtarefas/{id}', [SubtarefasController::class, 'destroy'])->name('subtarefas.destroy');
